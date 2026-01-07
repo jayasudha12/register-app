e5b89a329cd58b0104be877966e05522f0381b09
jayasudha12 <123159157+jayasudha12@users.noreply.github.com>
2026-01-07 14:56:31 +0530

    Update TestGreeter.java

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -4,9 +4,9 @@ import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
-import static org.junit.matchers.JUnitMatchers.containsString;
+
 
 public class TestGreeter {
 
